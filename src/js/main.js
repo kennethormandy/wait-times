686e393dba4d54afeb98bf9c294a4af12da50e98
Kenneth Ormandy <kenneth@chloi.io>
2014-03-25 03:38:32 -0700

    Try Vue.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,38 @@
+var request = require('browser-request');
 var domready = require('domready');
 var attachFastClick = require('fastclick');
+var Vue = require('vue');
 
-dug({
-  endpoint: 'http://www.kimonolabs.com/api/cutenv2y?apikey=4082701fda5e9ac55d10add137718ea6',
-  target: 'main',
-  error: function(e){
-    console.log(e);
-  },
-  beforeRender: function(pre){
-    console.log(name);
-  },
-  template: '<div>{{lastsuccess}}</div>\
-            <ul class="hList">\
-              {{#data}}\
-              <li class="hItem" data-heat="{{hTimeTwo.alt}}">\
-                <a class="hLink" href="{{hTitle.href}}">\
-                  <time class="hTime">{{value.hTimeOne.alt}}{{hTimeTwo.alt}}:{{hTimeThree.alt}}{{hTimeFour.alt}}</time>\
-                  <div class="hContent"><h2 class="hTitle">{{hTitle.text}}</h2>\
-                    <h3>{{data.login}}</h3>\
-                    <span class="hDetails">{{hDetails}}</span>\
-                  </div>\
-                </a>\
-              </li>\
-              {{/data}}\
-            </ul>'
+// var apiURL = 'http://www.kimonolabs.com/api/cutenv2y?apikey=4082701fda5e9ac55d10add137718ea6&callback=kimonoCallback';
+var apiURL = 'js/data.json';
+
+var render = new Vue({
+
+    el: '#main',
+
+    data: {
+      field: 'hTitle'
+    }
+
+    created: function () {
+      this.ascending = {}
+      this.$watch('sort', function () {
+        this.fetchData()
+      })
+    },
+
+    methods: {
+      fetchData: function () {
+        var xhr = new XMLHttpRequest();
+        var self = this;
+        xhr.open('GET', apiURL)
+        xhr.onload = function () {
+          self.hospitals = JSON.parse(xhr.responseText).results.data;
+        }
+        xhr.send()
+      }
+    }
+
 });
 
 domready(function () {
