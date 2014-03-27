7355592c902a2b3bab2f71dee9e4b5236d8eb86f
Kenneth Ormandy <kenneth@chloi.io>
2014-03-26 21:45:05 -0700

    Sort by wait or distance

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -5525,43 +5525,121 @@ var vm = new Vue({
 
     data: {
       key: 'wait',
-      reverse: false
+      reverse: false,
+      location: false
     },
 
-    created: function () {
-      this.fetchData()
+    created: function() {
+      this.fetchData();
     },
 
     methods: {
-      fetchData: function () {
+      fetchData: function() {
         var xhr = new XMLHttpRequest();
         var self = this;
         xhr.open('GET', apiURL);
         xhr.onload = function () {
           self.items = JSON.parse(xhr.responseText).results.data;
           self.items.forEach(function(h, i) {
-            h.title = h.hTitle.text;
+            h.name = h.hTitle.text;
             h.id = h.hTitle.href.split('rid=')[1];
             h.hours = h.hTimeOne.alt.toString() + h.hTimeTwo.alt.toString();
             h.minutes = h.hTimeThree.alt.toString() + h.hTimeFour.alt.toString();
             h.wait = h.hours + ':' + h.minutes;
-            h.details = h.hDetails;
+            if(h.hDetails) {
+              h.details = h.hDetails;
+            }
+            if(h.details && h.details.indexOf('This location is in ') !== -1) {
+              h.address = h.name + ', ' + h.details.split('This location is in ')[1] + ', AB, Canada';
+            } else if(h.name.split(' Community')[0] === 'Leduc') {
+              h.address = h.name + ', Leduc, AB, Canada';
+              h.details = 'This location is in Leduc';
+            } else if(h.name.split(' Community')[0] === 'Fork Sask') {
+              h.address = h.name + ', Fort Saskatchewan, AB, Canada';
+              h.details = 'This location is in Fort Saskatchewan';
+            } else {
+              h.address = h.name + ', Edmonton, AB, Canada';
+            }
           });
         }
         xhr.send();
       },
 
-      sort: function (msg, e) {
+      rank: function(msg, e) {
         e.targetVM.key = msg;
       },
 
+      locate: function(e) {
+        findMyLocation(e.targetVM.location, e.targetVM.$data.items);
+        // Should wait until findMyLocation is done
+        e.targetVM.rank('distance', e);
+      }
 
     }
 
 });
 
-domready(function () {
+domready(function() {
   attachFastClick(document.body);
 });
+
+function findMyLocation(location, items) {
+
+  var edmonton = new google.maps.LatLng(53.544389, -113.490927);
+  var browserSupportFlag = new Boolean();
+
+  // var myOptions = {
+  //   zoom: 6,
+  //   mapTypeId: google.maps.MapTypeId.ROADMAP
+  // };
+  // var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+
+  // Try W3C Geolocation (Preferred)
+  if(navigator.geolocation) {
+    browserSupportFlag = true;
+    navigator.geolocation.getCurrentPosition(function(position) {
+      location = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+      // map.setCenter(location);
+      findMyDistanceList(location, items);
+    }, function() {
+      handleNoGeolocation(browserSupportFlag);
+    });
+  }
+  // Browser doesn't support Geolocation
+  else {
+    browserSupportFlag = false;
+    handleNoGeolocation(browserSupportFlag);
+  }
+
+  function handleNoGeolocation(errorFlag) {
+    if (errorFlag == true) {
+      alert("Geolocation service failed. Sorting by distance from downtown Edmonton instead.");
+      location = edmonton;
+    } else {
+      alert("Sorry, your device does not support geolocation. Sorting by distance from downtown Edmonton instead.");
+      location = edmonton;
+    }
+    map.setCenter(initialLocation);
+  }
+
+}
+
+function findMyDistanceList(from, items) {
+  var geocoder = new google.maps.Geocoder();
+  items.forEach(function(h, i) {
+
+    geocoder.geocode( { 'address': h.address }, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        var to = results[0].geometry.location;
+        h.distance = google.maps.geometry.spherical.computeDistanceBetween(from, to);
+      }
+
+    });
+
+
+
+  });
+
+}
 
 },{"browser-request":1,"domready":2,"fastclick":3,"vue":23}]},{},[29])
