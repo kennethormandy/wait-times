6d0336131c561744fe00dd0c5bd2070b2ad298c2
Kenneth Ormandy <kenneth@chloi.io>
2014-03-26 18:54:26 -0700

    Sort by wait or name

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -5519,20 +5519,17 @@ var Vue = require('vue');
 // var apiURL = 'http://www.kimonolabs.com/api/cutenv2y?apikey=4082701fda5e9ac55d10add137718ea6&callback=kimonoCallback';
 var apiURL = 'js/data.json';
 
-var render = new Vue({
+var vm = new Vue({
 
     el: '#main',
 
     data: {
-      key: 'time',
+      key: 'wait',
       reverse: false
     },
 
     created: function () {
-      this.ascending = {}
-      // this.$watch('sort', function () {
-        this.fetchData()
-      // })
+      this.fetchData()
     },
 
     methods: {
@@ -5547,12 +5544,18 @@ var render = new Vue({
             h.id = h.hTitle.href.split('rid=')[1];
             h.hours = h.hTimeOne.alt.toString() + h.hTimeTwo.alt.toString();
             h.minutes = h.hTimeThree.alt.toString() + h.hTimeFour.alt.toString();
-            h.time = h.hours + ':' + h.minutes;
+            h.wait = h.hours + ':' + h.minutes;
             h.details = h.hDetails;
           });
         }
         xhr.send();
-      }
+      },
+
+      sort: function (msg, e) {
+        e.targetVM.key = msg;
+      },
+
+
     }
 
 });
